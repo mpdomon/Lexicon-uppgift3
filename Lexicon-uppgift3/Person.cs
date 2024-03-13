5975b722cb9687be313c5a7be06e3806151d6904
mpdomon <martin.domon@gmail.com>
2024-03-13 09:07:54 +0100

    Add project files.

---
+++ Lexicon-uppgift3/Person.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_uppgift3
+{
+    internal class Person
+    {
+        private int age1;
+        private string firstName;
+        private string lastName;
+        private int height1;
+        private int weight1;
+        public int Age
+        {
+            get { return age1; }
+            set
+            {
+                if (value > 0)
+                { age1 = value; }
+                else
+                {
+                    throw new ArgumentException("Error: Use a number higher than 0");
+                }
+            }
+        }
+        public string fName
+        {
+            get { return firstName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length >= 2 && value.Length <= 10)
+                {
+                    firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name can't be shorter than 2 or longer than 10 letters.");
+                }
+            }
+        }
+        public string lName
+        {
+            get { return lastName; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && value.Length >= 3 && value.Length <= 15)
+                {
+                    lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name can't be shorter than 3 or longer than 15 letters.");
+                }
+            }
+        }
+        public int height
+        {
+            get { return height1; }
+            set
+            {
+                if (value > 0)
+                {
+                    height1 = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Height must be higher than 0");
+                }
+            }
+        }
+        public int weight
+        {
+            get { return weight1; }
+            set
+            {
+                if (value > 0)
+                {
+                    weight1 = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Weight must be higher than 0");
+                }
+            }
+
+
+        }
+    }
+}
+
---
+++ Lexicon-uppgift3/PersonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexicon_uppgift3
+{
+    internal class PersonHandler {
+
+
+        public Person CreatePerson(int age, string fname, string lname, int height, int weight) 
+        { 
+            Person person = new Person();
+            person.Age = age;
+            person.fName = fname;
+            person.lName = lname;
+            person.height = height;
+            person.weight = weight;
+
+            return person;
+        }
+
+
+        public void setAge (Person pers, int age) {
+            pers.Age = age;
+        }
+    }
+}
